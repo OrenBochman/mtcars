f58151e822ccc6f42218a637e13e00dc12706939
OrenBochman <OrenBochman@gmail.com>
2018-05-03 12:38:24 +0300

    shiny dashboard version

--- app.R
+++ app.R
@@ -8,29 +8,51 @@
 #
 
 library(shiny)
+library(shinydashboard)
+
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+ui <- dashboardPage(
+  dashboardHeader(title = "Old Faithful Geyser Data"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("distPlot", height = 250)),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+      box(
+        title = "Controls",
+        sliderInput("bins",
+                    "Number of bins:",
+                    min = 1,
+                    max = 50,
+                    value = 30)      )
+    )
+  )
 )
+
+# ui_old <- fluidPage(
+#    
+#    # Application title
+#    titlePanel("Old Faithful Geyser Data"),
+#    
+#    # Sidebar with a slider input for number of bins 
+#    sidebarLayout(
+#       sidebarPanel(
+#          sliderInput("bins",
+#                      "Number of bins:",
+#                      min = 1,
+#                      max = 50,
+#                      value = 30)
+#       ),
+#       
+#       # Show a plot of the generated distribution
+#       mainPanel(
+#          plotOutput("distPlot")
+#       )
+#    )
+# )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
